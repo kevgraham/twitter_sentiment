2625d9ef87f24df05f6e72b9b6dec302cecb6812
Kevin Graham <kevin.edward.graham@gmail.com>
2018-04-04 10:41:16 +0800

    refactor

--- src/main/java/twitter_sentiment/mappers/TweetSentimentMapper.java
+++ src/main/java/twitter_sentiment/mappers/TweetSentimentMapper.java
@@ -16,25 +16,18 @@ public interface TweetSentimentMapper {
      * Tweet Table
      */
 
-    final String INSERT_TWEET = "INSERT INTO `TwitterSentiment`.`Tweets` " +
-                                "(`tweet`, `anger`, `fear`, `joy`, `sadness`, `analytical`, `confident`, `tentative`) " +
-                                "VALUES (#{tweet}, #{anger}, #{fear}, #{joy}, #{sadness}, #{analytical}, #{confident}, #{tentative}); ";
-    @Insert(INSERT_TWEET)
+    @Insert("INSERT INTO `TwitterSentiment`.`Tweets` " +
+            "(`tweet`, `anger`, `fear`, `joy`, `sadness`, `analytical`, `confident`, `tentative`) " +
+            "VALUES (#{tweet}, #{anger}, #{fear}, #{joy}, #{sadness}, #{analytical}, #{confident}, #{tentative})")
     public int insertTweet(TweetSentiment tweetSentiment);
 
-
-    final String FIND_TWEETS_BY_TONE = "SELECT * FROM `TwitterSentiment`.`Tweets` WHERE ${tone} > .75; ";
-    @Select(FIND_TWEETS_BY_TONE)
+    @Select("SELECT * FROM `TwitterSentiment`.`Tweets` WHERE ${tone} > .75")
     public ArrayList<TweetSentiment> findTweetsByTone(@Param("tone") String tone);
 
-
-    final String FIND_SPECIFIC_TWEET = "SELECT * FROM `TwitterSentiment`.`Tweets` WHERE tweet = #{text} LIMIT 1; ";
-    @Select(FIND_SPECIFIC_TWEET)
+    @Select("SELECT * FROM `TwitterSentiment`.`Tweets` WHERE tweet = #{text} LIMIT 1")
     public TweetSentiment findSpecificTweet(String text);
 
-
-    final String FIND_ID_BY_TWEET = "SELECT `id` FROM `TwitterSentiment`.`Tweets` WHERE tweet = #{text}; ";
-    @Select(FIND_ID_BY_TWEET)
+    @Select("SELECT `id` FROM `TwitterSentiment`.`Tweets` WHERE tweet = #{text}")
     public int findIdByTweet(String text);
 
 
@@ -42,20 +35,15 @@ public interface TweetSentimentMapper {
      * User Table
      */
 
-    final String INSERT_USER = "INSERT INTO `TwitterSentiment`.`Users` " +
-                               "(`screen_name`, `name`) " +
-                               "VALUES (#{screen_name}, #{name});";
-    @Insert(INSERT_USER)
+    @Insert("INSERT INTO `TwitterSentiment`.`Users` " +
+            "(`screen_name`, `name`) " +
+            "VALUES (#{screen_name}, #{name})")
     public int insertUser(User user);
 
-
-    final String FIND_USER_BY_SCREENNAME = "SELECT * FROM TwitterSentiment.Users WHERE `screen_name` = #{screen_name} LIMIT 1";
-    @Select(FIND_USER_BY_SCREENNAME)
+    @Select("SELECT * FROM TwitterSentiment.Users WHERE `screen_name` = #{screen_name} LIMIT 1")
     public User findUserByScreenName(String screen_name);
 
-
-    final String FIND_ID_BY_SCREENNAME = "SELECT `id` FROM TwitterSentiment.Users WHERE `screen_name` = #{screen_name}";
-    @Select(FIND_ID_BY_SCREENNAME)
+    @Select("SELECT `id` FROM TwitterSentiment.Users WHERE `screen_name` = #{screen_name}")
     public int findIdByScreenName(String screen_name);
 
 
@@ -63,15 +51,12 @@ public interface TweetSentimentMapper {
      * TweetsUser Table
      */
 
-    final String INSERT_USER_TWEET = "INSERT INTO `TwitterSentiment`.`TweetsUsers` (`user_id`,`tweet_id`) VALUES (${user_id},${tweet_id});";
-    @Insert(INSERT_USER_TWEET)
+    @Insert("INSERT INTO `TwitterSentiment`.`TweetsUsers` (`user_id`,`tweet_id`) VALUES (${user_id},${tweet_id})")
     public int insertUserTweet(@Param("user_id") int user_id, @Param("tweet_id") int tweet_id);
 
-
-    final String FIND_TWEETS_BY_USER = "SELECT * FROM `TwitterSentiment`.`Users` u JOIN `TwitterSentiment`.`TweetsUsers` tu " +
-                                       "ON u.id = tu.user_id JOIN `TwitterSentiment`.`Tweets` t ON tu.tweet_id = t.id " +
-                                       "WHERE `screen_name` = \"${user}\";";
-    @Select(FIND_TWEETS_BY_USER)
+    @Select("SELECT * FROM `TwitterSentiment`.`Users` u JOIN `TwitterSentiment`.`TweetsUsers` tu " +
+            "ON u.id = tu.user_id JOIN `TwitterSentiment`.`Tweets` t ON tu.tweet_id = t.id " +
+            "WHERE `screen_name` = \"${user}\"")
     ArrayList<TweetSentiment> findTweetsByUser(@Param("user") String user);
 
 }
